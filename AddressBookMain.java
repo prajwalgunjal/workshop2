6c067ec84642ef28873dbac35f969a17e95e8a61
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-16 11:10:28 +0530

    address book updtaed

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -5,10 +5,11 @@ public class AddressBookMain {
         AddressBook addressBook = new AddressBook();
         AddressBook addressBook2 = new AddressBook();
         AddressBook[] Address = new AddressBook[10];
+        AddressBook[] Address2 = new AddressBook[10];
         int count=0;
         while(true) {
         System.out.println("What you want to perforem");
-        System.out.println("1) add contact \n 2) add new address book\n3 exit ");
+        System.out.println("1) add contact \n 2) add new address book\n3 exit 4) print from address book 1 5");
         Scanner sc = new Scanner(System.in);
         int choice=sc.nextInt();
             switch (choice) {
@@ -21,15 +22,22 @@ public class AddressBookMain {
                 case 2 -> {
                     addressBook2.addContact();
                     System.out.println(addressBook2);
+                    Address2[count]=addressBook2;
                     //addressBook2.display();
                 }
                 case 3 -> {
                     System.exit(0);
                 }
                 case 4 -> {
-                    System.out.println("Enter which contatc to be printed ");
+                    System.out.println("Enter which contatc to be printed for address one ");
                     int find = sc.nextInt();
                         System.out.println(Address[find]);
+                }
+                case 5 -> {
+                    System.out.println("for 2nd address printing ");
+                    System.out.println("Enter which contatc to be printed for address one ");
+                    int find2 = sc.nextInt();
+                    System.out.println(Address2[find2]);
                 }
             }
         }
